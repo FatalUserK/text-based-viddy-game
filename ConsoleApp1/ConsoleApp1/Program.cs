0205cce8fb74e036f3f1ccb635086267c942bf05
ESCG\534301 <534301@escg.ac.uk>
2022-09-26 16:57:52 +0100

    updated it with some random typing speed text, still working on splitting up a string into separate print lines

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,24 +24,24 @@ class Program
         Thread.Sleep(1500);
 
         Print("...", 1500);
-        Thread.Sleep(3000);
+        Thread.Sleep(2000);
 
         // "Wow, look! What a cool game" is a meta reference showing that this game is likely to be less formal with its humor and story since its acknowledging itself as a game
 
         Print("\n\nI wonder\n", 15);
-        Thread.Sleep(700);
+        Thread.Sleep(500);
         Print("         ", 0);
         Print("if anyone's\n", 15);
-        Thread.Sleep(700);
+        Thread.Sleep(500);
         Print("                     ", 0);
         Print("going to\n", 15);
-        Thread.Sleep(700);
+        Thread.Sleep(500);
         Print("                              ", 0);
         Print("make it\n", 15);
-        Thread.Sleep(1400);
+        Thread.Sleep(800);
         Print("                                      ", 0);
         Print(":)\n", 5);
-        Thread.Sleep(2000);
+        Thread.Sleep(1500);
 
         // ^ this is initially just a funny little intro i made for fun but i could give it some meaning later on or in a later iteration of this project, such as the game ending with everyone dying, showing that nobody is "going to make it"
 
@@ -51,7 +51,7 @@ class Program
         // The User is probably unaware of this and will likely bring some sort of gratification knowing this choice had some sort of payoff in the long run,
         // This could also lead to some comedic moments later on deciding on the input the user decided on
 
-        Console.WriteLine("\n\n\n\n[Type anything START]");
+        Console.WriteLine("\n\n\n\n[Type anything START]\n");
 
 
         // randomise list set up in the event the user does not enter inputs when required
@@ -82,16 +82,74 @@ class Program
         Print(userInputEcho, 0);
         Print(startInput, 0);
         Print("", 1500);
-        Print("... ", 1500);
+        Print("...", 1500);
         Console.WriteLine();
 
         Thread.Sleep(3000);
 
-        Print("Trace Discovered...\n", randNumber);
-        Print("Locking onto Source...\n", 300);
+        Print("\n\nWARNING! USER INPUT DETECTED,", 2);
+        Thread.Sleep(1200);
+        Print(" SENDING REQUEST FOR RESPONSE 221B", 2);
+        Print("...", 800);
 
-        
+        Thread.Sleep(1500);
 
+        Print("\n\nRESPONSE RECEIVED, INITIALISING OIS", 2);
+        Print("...", 1200);
+
+
+
+
+
+
+        Print("Operator Interaction System Initialised...\n\n", 10);
+
+        Thread.Sleep(1200);
+
+        string textTemp = "Analysing Trace...\n";               // sets the text that will be sent
+        foreach (char c in textTemp)                            // runs the script for every letter left in the designated string [textTemp]
+        {
+            randNumber = rnd.Next(5, 100);                      // picks a random number between 5 and 500
+            Print(c.ToString(), randNumber);                    // sends the next letter from [textTemp] that hasnt already been sent and then make the thread sleep the by the amount designated by the random number above
+
+
+        }
+        Thread.Sleep(2000);
+
+        textTemp = "Trace Discovered...\n";
+        foreach (char c in textTemp)
+        {
+            randNumber = rnd.Next(5, 100);
+            Print(c.ToString(), randNumber);
+
+
+        }
+        Thread.Sleep(2000);
+
+        textTemp = "Locking onto Source...\n";
+        foreach (char c in textTemp)
+        {
+            randNumber = rnd.Next(5, 100);
+            Print(c.ToString(), randNumber);
+
+
+        }
+        Thread.Sleep(2000);
+        textTemp = "Entity Located, sending EDCP";
+        foreach (char c in textTemp)
+        {
+            randNumber = rnd.Next(10, 200);
+            Print(c.ToString(), randNumber);
+
+
+        }
+        Print("...\n", 1000);
+
+        textTemp = "54686520  456e6420  6f662047  6f642077\n696c6c20  42656661  6c6c2075  7320416c\n6c0d0a41  73207765  2044616e  63652062\n65666f72  65206869  7320436f  72707365\n20696e20  74686520  48616c6c  206f6620\n54687261  6c6c0d0a  42652073  74696c6c\n2c20616e  64206769  76652075  70204574\n65726e61  6c205265  73740d0a  4173206f\n75722044  61726b20  54686f75  67687473\n20626567  696e2074  6f204665  7374\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+
+
+        Thread.Sleep(3000);
     }
     public static void Print(string text, int speed)
         {
